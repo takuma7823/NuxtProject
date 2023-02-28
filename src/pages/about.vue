cebab1c34abaab1231f333eeaae36302377e431a
takumi kosaka <takumikosaka@takuminoMacBook-Pro.local>
2023-02-28 22:27:13 +0900

    eslint, prettier の設定をおこなった

--- src/pages/about.vue
+++ src/pages/about.vue
@@ -1,73 +1,70 @@
 <script setup lang="ts">
-  const runtimeConfig = useRuntimeConfig()
-  const apiKey = runtimeConfig.public.apiKey
-  const photoUrlFond = 'https://maps.googleapis.com/maps/api/place/photo?maxwidth=400'
-  const radius = ref<string>('')
-  const storeIndex = ref<number>(0)
-  const storeArray = ref<string[]>([])
-  let storeInfo = reactive( {data: null} )
+const runtimeConfig = useRuntimeConfig();
+const apiKey = runtimeConfig.public.apiKey;
+const photoUrlFond = 'https://maps.googleapis.com/maps/api/place/photo?maxwidth=400';
+const radius = ref<string>('');
+const storeIndex = ref<number>(0);
+const storeArray = ref<string[]>([]);
+let storeInfo = reactive({ data: null });
 
-  const searchStore = async () => {
-    const url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?';
-    const params = new URLSearchParams({
-      key: apiKey,
-      location: '35.78928207428572, 139.4560333981599',
-      radius: '30',
-      type: 'store',
-      language: 'ja'
-    })
-    const res = await fetch(url + params.toString())
-    // CORS問題発生
-    console.log('res',res)
+const searchStore = async () => {
+  const url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?';
+  const params = new URLSearchParams({
+    key: apiKey,
+    location: '35.78928207428572, 139.4560333981599',
+    radius: '30',
+    type: 'store',
+    language: 'ja',
+  });
+  const res = await fetch(url + params.toString());
+  // CORS問題発生
+  console.log('res', res);
+};
+
+const { data: data } = await useFetch('https://maps.googleapis.com/maps/api/place/nearbysearch/json', {
+  params: {
+    key: apiKey,
+    location: '35.78928207428572, 139.4560333981599',
+    radius: '30',
+    type: 'store',
+    language: 'ja',
+  },
+});
+storeArray.value = data.value.results;
+storeInfo.data = storeArray.value[storeIndex.value];
+
+const getStorePhotoUrl = computed((): string => {
+  if (storeInfo.data.hasOwnProperty('photos')) {
+    const photoReference = storeInfo.data.photos[0].photo_reference;
+
+    return photoUrlFond + '&photoreference=' + photoReference + '&key=' + apiKey;
   }
 
-  const { data: data } = await useFetch(
-    'https://maps.googleapis.com/maps/api/place/nearbysearch/json',
-    {
-      params: {
-        key: apiKey,
-        location: '35.78928207428572, 139.4560333981599',
-        radius: '30',
-        type: 'store',
-        language: 'ja'
-      }
-    }
-  )
-  storeArray.value = data.value.results
-  storeInfo.data = storeArray.value[storeIndex.value]
+  return '';
+});
 
-  const getStorePhotoUrl = computed((): string => {
-    if (storeInfo.data.hasOwnProperty('photos')) {
-      const photoReference = storeInfo.data.photos[0].photo_reference
+const getStoreName = computed((): string => {
+  return storeInfo.data.name;
+});
 
-      return photoUrlFond + '&photoreference=' + photoReference + '&key=' + apiKey
-    }
-
-    return ''
-  })
-
-  const getStoreName = computed((): string => {
-    return storeInfo.data.name
-  })
-
-  const changeStore = (): void => {
-    storeIndex.value = storeIndex.value + 1
-    storeInfo.data = storeArray.value[storeIndex.value]
-  }
+const changeStore = (): void => {
+  storeIndex.value = storeIndex.value + 1;
+  storeInfo.data = storeArray.value[storeIndex.value];
+};
 </script>
 
 <template>
   <div class="about">
-    <NuxtLink :to="`/`">index.vue</NuxtLink>
+    <NuxtLink :to="`/`">index. vue</NuxtLink>
 
     <div class="search">
-      <input v-model="radius" type="text" class="radius">
+      <input v-model="radius" type="text" class="radius" />
       <button @click="searchStore()">検索</button>
     </div>
 
     <div v-if="storeInfo" class="result">
-      <p>{{ getStoreName }} </p>
-      <img v-if="getStorePhotoUrl" :src="getStorePhotoUrl">
+      <p>{{ getStoreName }}</p>
+      <img v-if="getStorePhotoUrl" :src="getStorePhotoUrl" />
       <p v-else>No Image</p>
       <button @click="changeStore()">Next</button>
     </div>
@@ -75,10 +72,10 @@
 </template>
 
 <style lang="scss" scoped>
-  .about {
-    margin: 50px auto;
-  }
-  .radius {
-    border: solid 1px;
-  }
+.about {
+  margin: 50px auto;
+}
+.radius {
+  border: solid 1px;
+}
 </style>
