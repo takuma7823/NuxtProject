38164bda6fe8122d86534b2f70377aa757ec20e2
koosaka <125739255+koosaka@users.noreply.github.com>
2023-02-28 22:09:57 +0900

    最新のsetUpの書き方に修正 (#1)
    Co-authored-by: takumi kosaka <takumikosaka@takuminoMacBook-Pro.local>

--- src/pages/about.vue
+++ src/pages/about.vue
@@ -1,3 +1,61 @@
+<script setup lang="ts">
+  const runtimeConfig = useRuntimeConfig()
+  const apiKey = runtimeConfig.public.apiKey
+  const photoUrlFond = 'https://maps.googleapis.com/maps/api/place/photo?maxwidth=400'
+  const radius = ref<string>('')
+  const storeIndex = ref<number>(0)
+  const storeArray = ref<string[]>([])
+  let storeInfo = reactive( {data: null} )
+
+  const searchStore = async () => {
+    const url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?';
+    const params = new URLSearchParams({
+      key: apiKey,
+      location: '35.78928207428572, 139.4560333981599',
+      radius: '30',
+      type: 'store',
+      language: 'ja'
+    })
+    const res = await fetch(url + params.toString())
+    // CORS問題発生
+    console.log('res',res)
+  }
+
+  const { data: data } = await useFetch(
+    'https://maps.googleapis.com/maps/api/place/nearbysearch/json',
+    {
+      params: {
+        key: apiKey,
+        location: '35.78928207428572, 139.4560333981599',
+        radius: '30',
+        type: 'store',
+        language: 'ja'
+      }
+    }
+  )
+  storeArray.value = data.value.results
+  storeInfo.data = storeArray.value[storeIndex.value]
+
+  const getStorePhotoUrl = computed((): string => {
+    if (storeInfo.data.hasOwnProperty('photos')) {
+      const photoReference = storeInfo.data.photos[0].photo_reference
+
+      return photoUrlFond + '&photoreference=' + photoReference + '&key=' + apiKey
+    }
+
+    return ''
+  })
+
+  const getStoreName = computed((): string => {
+    return storeInfo.data.name
+  })
+
+  const changeStore = (): void => {
+    storeIndex.value = storeIndex.value + 1
+    storeInfo.data = storeArray.value[storeIndex.value]
+  }
+</script>
+
 <template>
   <div class="about">
     <NuxtLink :to="`/`">index.vue</NuxtLink>
@@ -15,79 +73,6 @@
     </div>
   </div>
 </template>
-
-<script lang="ts">
-// import { reactive } from 'vue'
-
-export default ({
-  async setup() {
-    const runtimeConfig = useRuntimeConfig()
-    const apiKey = runtimeConfig.public.apiKey
-    const photoUrlFond = 'https://maps.googleapis.com/maps/api/place/photo?maxwidth=400'
-    const radius = ref<string>('')
-    const storeIndex = ref<number>(0)
-    const storeArray = ref<string[]>([])
-    let storeInfo = reactive( {data: null} )
-
-    const searchStore = async () => {
-      const url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?';
-      const params = new URLSearchParams({
-        key: apiKey,
-        location: '35.78928207428572, 139.4560333981599',
-        radius: '30',
-        type: 'store',
-        language: 'ja'
-      })
-      const res = await fetch(url + params.toString())
-      // CORS問題発生
-      console.log('res',res)
-    }
-
-    const { data: data } = await useFetch(
-      'https://maps.googleapis.com/maps/api/place/nearbysearch/json',
-      {
-        params: {
-          key: apiKey,
-          location: '35.78928207428572, 139.4560333981599',
-          radius: '30',
-          type: 'store',
-          language: 'ja'
-        }
-      }
-    )
-    storeArray.value = data.value.results
-    storeInfo.data = storeArray.value[storeIndex.value]
-
-    const getStorePhotoUrl = computed((): string => {
-      if (storeInfo.data.hasOwnProperty('photos')) {
-        const photoReference = storeInfo.data.photos[0].photo_reference
-
-        return photoUrlFond + '&photoreference=' + photoReference + '&key=' + apiKey
-      }
-
-      return ''
-    })
-
-    const getStoreName = computed((): string => {
-      return storeInfo.data.name
-    })
-
-    const changeStore = (): void => {
-      storeIndex.value = storeIndex.value + 1
-      storeInfo.data = storeArray.value[storeIndex.value]
-    }
-
-    return {
-      getStoreName,
-      getStorePhotoUrl,
-      changeStore,
-      storeInfo,
-      radius,
-      searchStore
-    }
-  }
-})
-</script>
 
 <style lang="scss" scoped>
   .about {
