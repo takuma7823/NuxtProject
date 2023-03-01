2d157598b8aa20ad7931d23bb03777c60ac491a5
takumi kosaka <takumikosaka@takuminoMacBook-Pro.local>
2023-03-01 21:07:48 +0900

    searchページの追加

---
+++ src/pages/search.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="search">検索画面</div>
+</template>
+
+<style lang="scss" scoped>
+.search {
+  height: 100svh;
+  width: 100svh;
+  background-color: white;
+}
+</style>
