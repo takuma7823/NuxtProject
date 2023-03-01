85171d954c641bb01fe5c256363495a83e3c5226
koosaka <125739255+koosaka@users.noreply.github.com>
2023-03-01 17:53:06 +0900

    atomicなフォルダの作成と、post-cardコンポーネントの大枠作成 (#3)
    * atomicフォルダの作成
    とpost-cardの作成
    
    * indexファイルの変更を取り消した#
    
    ---------
    
    Co-authored-by: takumi kosaka <takumikosaka@takuminoMacBook-Pro.local>

---
+++ src/components/organisms/post-card.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+type PostCardProps = {
+  logo: string;
+  storeName: string;
+  storeNameKana: string;
+  Images: string[];
+  distance: string;
+  openTime: string;
+  closeTime: string;
+  minPrice: string;
+  maxPrice: string;
+  discription: Text;
+};
+
+// MEMO propsを渡すときにコメントアウト外す
+// defineProps<PostCardProps>();
+</script>
+
+<template>
+  <v-card class="mx-auto" max-width="600">
+    <v-card-title> カフェ国木田 </v-card-title>
+    <v-card-subtitle> cafe kunikida </v-card-subtitle>
+    <v-img src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg" height="600px" cover></v-img>
+
+    <v-card-subtitle> 1,000 miles of wonder </v-card-subtitle>
+    <v-card-subtitle> 1,000 miles of wonder </v-card-subtitle>
+    <v-card-subtitle> 1,000 miles of wonder </v-card-subtitle>
+
+    <v-card-text>
+      I'm a thing. But, like most politicians, he promised more than he could deliver. You won't have time for sleeping,
+      soldier, not with all the bed making you'll be doing. Then we'll go with that data file! Hey, you add a one and
+      two zeros to that or we walk! You're going to do his laundry? I've got to find a way to escape.
+    </v-card-text>
+  </v-card>
+</template>
+
+<style lang="scss" scoped></style>
