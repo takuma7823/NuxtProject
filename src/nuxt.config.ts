9d46186fdfb8d54e428f66ac94d743037307ad8d
takumi kosaka <takumikosaka@takuminoMacBook-Pro.local>
2023-03-01 19:55:23 +0900

    ファイル名の修正,HomeFooterの作成

--- src/nuxt.config.ts
+++ src/nuxt.config.ts
@@ -1,23 +1,25 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import svgLoader from 'vite-svg-loader';
 export default defineNuxtConfig({
-  css: [
-    'vuetify/lib/styles/main.sass',
-    'mdi/css/materialdesignicons.min.css',
-    '~/assets/scss/reset.scss'
-  ],
+  css: ['vuetify/lib/styles/main.sass', 'mdi/css/materialdesignicons.min.css', '~/assets/scss/reset.scss'],
   build: {
     transpile: ['vuetify'],
   },
   vite: {
     define: {
       'process.env.DEBUG': false,
-    }
+    },
+    plugins: [
+      svgLoader({
+        svgo: false,
+      }),
+    ],
   },
   buildModules: ['@nuxt/typescript-build'],
   runtimeConfig: {
     public: {
       apiKey: '',
       apiUrl: '',
-    }
+    },
   },
-})
+});
